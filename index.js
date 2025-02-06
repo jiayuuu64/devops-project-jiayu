5ba6bb313c854434c972cbf4023295a02f95aaf2
jiayuuu64 <2305136J@student.tp.edu.sg>
2025-02-06 14:39:55 +0800

    Added additional features (pm2 and promethenus

--- index.js
+++ index.js
@@ -1,27 +1,42 @@
 const express = require('express');
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
+const promClient = require('prom-client');
 require('dotenv').config();
  
 const app = express();
 const PORT = process.env.PORT || 5050;
  
+// Create Prometheus Registry
+const register = new promClient.Registry();
+
+// Default metrics (CPU, memory, etc.)
+promClient.collectDefaultMetrics({ register });
+
+// Define your own custom metrics (example)
+const jobSearchCounter = new promClient.Counter({
+    name: 'job_search_requests_total',
+    help: 'Total number of job search requests'
+});
+ 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(express.static("./public"));
- 
+
 const statusMonitor = require('express-status-monitor');
 app.use(statusMonitor());
 
 // Connect to MongoDB
 mongoose.connect(process.env.DB_CONNECT)
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Failed to connect to MongoDB', err));
 
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error('Failed to connect to MongoDB', err));
- 
 // Import job-related functions
 const { searchJobs } = require('./utils/search-job');
-app.get('/search-jobs', searchJobs);
+app.get('/search-jobs', (req, res) => {
+    jobSearchCounter.inc(); // Increment job search counter
+    searchJobs(req, res);
+});
 
 const { addJob } = require('./utils/create-job');
 app.post('/add-job', addJob);
@@ -35,7 +50,13 @@ app.get('/view-jobs', viewJobs);
 
 const { applyjob } = require('./utils/applyjob');
 app.post('/apply-job/:jobId', applyjob);
- 
+
+// Metrics endpoint
+app.get('/metrics', async (req, res) => {
+    res.set('Content-Type', register.contentType);
+    res.end(await register.metrics());
+});
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + "/public/index.html");
 });
